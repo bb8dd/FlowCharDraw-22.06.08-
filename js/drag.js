07582c192bcf66d95643f97e73122fbfcd29ac7a
ldH <ldh19971027@gmail.com>
2022-06-10 17:22:58 +0900

    마우스 따라다니는 것까지

--- js/drag.js
+++ js/drag.js
@@ -1,1 +1,32 @@
-const floder = document.querySelector(".folder");
+const folder = document.querySelector(".folder");
+let isPress = false;
+
+let positionX;
+let positionY;
+
+const folderDribble = () => {
+    isPress = true;
+};
+
+const folderDribbleStop = (e) => {
+    isPress = false;
+}
+
+folder.addEventListener("mousedown", folderDribble);
+folder.addEventListener("mouseup", folderDribbleStop);
+
+const folderMove = () => {
+    setTimeout(() => {
+        if (isPress) {
+            window.addEventListener("mousemove", (e) => {
+                let positionX = e.clientX;
+                let positionY = e.clientY;
+                folder.style.left = `${positionX}px`;
+                folder.style.top = `${positionY}px`;
+                console.log(positionX, positionX);
+            })
+        }
+    },10);
+}
+
+window.addEventListener("load", folderMove);
