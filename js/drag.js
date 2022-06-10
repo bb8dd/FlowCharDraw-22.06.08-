8307ee8ed1021be42f776b26827419b1d4e6db61
ldH <ldh19971027@gmail.com>
2022-06-10 17:54:26 +0900

    마우스 멈춰야 함

--- js/drag.js
+++ js/drag.js
@@ -4,29 +4,37 @@ let isPress = false;
 let positionX;
 let positionY;
 
+const move = () => {
+    if (isPress) {
+        console.log("들어오고 있음", isPress)
+        window.addEventListener("mousemove", (e) => {
+            let positionX = e.clientX;
+            let positionY = e.clientY;
+            folder.style.left = `${positionX}px`;
+            folder.style.top = `${positionY}px`;
+            console.log(positionX, positionX);
+        })
+    } else {
+        console.log("들어오고 있음", isPress)
+        clearInterval(mouseMove);
+    }
+}
+
 const folderDribble = () => {
     isPress = true;
+    setInterval(move, 100);
 };
 
-const folderDribbleStop = (e) => {
+const folderDribbleStop = () => {
+    console.log("들어옴")
     isPress = false;
+    if(!isPress) {
+        console.log("들어옴!!!!!!!!!!!!!!!!!!!!!!")
+        clearInterval(mouseMove);
+    }
 }
+
+const mouseMove = setInterval(move, 100);
 
 folder.addEventListener("mousedown", folderDribble);
-folder.addEventListener("mouseup", folderDribbleStop);
-
-const folderMove = () => {
-    setTimeout(() => {
-        if (isPress) {
-            window.addEventListener("mousemove", (e) => {
-                let positionX = e.clientX;
-                let positionY = e.clientY;
-                folder.style.left = `${positionX}px`;
-                folder.style.top = `${positionY}px`;
-                console.log(positionX, positionX);
-            })
-        }
-    },10);
-}
-
-window.addEventListener("load", folderMove);
+window.addEventListener("mouseup", folderDribbleStop);
